50fdca5c9bd77d1559bb8982caedc8043652466f
Faissen <224894931+Faissen@users.noreply.github.com>
2026-01-10 03:56:19 +0000

    Refactor data loading and insertion logic in import_to_pgsql.py

--- import_to_pgsql.py
+++ import_to_pgsql.py
@@ -7,8 +7,11 @@ import os # To access environment variables
 # Load environment variables from .env file
 load_dotenv()
 
-# Read cleaned data
-df = pd.read_csv('cleaned_superstore_data.csv')
+# Load cleaned data
+df = pd.read_csv('superstore_cleaned.csv')
+df["order_date"] = pd.to_datetime(df["order_date"], errors="coerce") #Convert due to null values, prevent errors
+df["ship_date"] = pd.to_datetime(df["ship_date"], errors="coerce") #Convert due to null values, prevent errors
+schema = "superstore"
 
 # Connect to PostgreSQL database
 conn = psycopg2.connect(
@@ -32,22 +35,37 @@ tables = {
                   "date_id", "ship_mode_id"],
 }
 
-def insert_data(table, columns, df):
+def insert_data(schema, table, columns):
     # Create the INSERT INTO SQL query
     cols = ",".join(columns).strip() # Column names for the query, remove any extra spaces
     placeholders = ", ".join(["%s"] * len(columns)) # Placeholder for parameterized query, %s for each column
     query = f"""
-    INSERT INTO {table} ({cols})
-    VALUES ({placeholders});"""
+    INSERT INTO {schema}.{table} ({cols})
+    VALUES ({placeholders})
+    ON CONFLICT DO NOTHING;"""
     return query
     
 # Insert each row into the table
 for table, columns in tables.items(): # Iterate over each table and its columns
-    insert_query = insert_data(table, columns, df) # Get the insert query for the table
-    for _, row in df.iterrows(): # Iterate over each row in the DataFrame
+# FACT TABLE → insert all rows 
+    if table == "fact_sales": 
+        df_subset = df[columns] 
+# DIMENSIONS → insert only unique rows 
+    elif table == "dim_date":
+        df_subset = df[columns].drop_duplicates()
+        df_subset = df_subset[df_subset["date_id"].notna()] # Remove invalid rows 
+    else:
+        df_subset = df[columns].drop_duplicates()
+
+    insert_query = insert_data(schema, table, columns) # Get the insert query for the table
+    for _, row in df_subset.iterrows(): # Iterate over each row in the DataFrame
         values = [] # List to hold the values for the current row
         for col in columns:
-            values.append(row[col])
+            val = row[col] 
+            # Convert NaN to None for PostgreSQL 
+            if pd.isna(val):  # Check if the value is NaN
+                val = None # Set to None for PostgreSQL
+            values.append(val) # Append the value to the list
         cur.execute(insert_query, values)
 
 # Commit changes and close the connection
